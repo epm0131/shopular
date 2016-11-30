7d4b676fa47ba0444765db870580e31427c89bb9
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-30 14:48:13 -0500

    I have my table show and able to add items when the data is in the service

--- js/shop.controller.js
+++ js/shop.controller.js
@@ -4,30 +4,16 @@
   angular.module('shopular')
     .controller('InventoryController', InventoryController);
 
-    InventoryController.$inject = [ 'storageService' ];
+    InventoryController.$inject = [ 'StorageService' ];
 
 
-    function InventoryController() {
+    function InventoryController(storageService) {
+
+      this.allData = storageService.getAll();
 
       this.newItem = {};
       this.tax = 1.0575;
       this.orderBy = "-name";
-
-      this.items = [
-        { "id": 2957, "name": "Widget", "price": 32, "quantity": 203, "color": "Red", "discount": 31 },
-        { "id": 89274, "name": "Golf club", "price": 98, "quantity": 10, "color": "Black", "discount": 0 },
-        { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "White", "discount": 0 },
-        { "id": 87363, "name": "Bonzai tree", "price": 76, "quantity": 2, "color": "Green", "discount": 0 },
-        { "id": 1736, "name": "Towel", "price": 55, "quantity": 30, "color": "Brown", "discount": 10 },
-        { "id": 4836, "name": "Dog bed", "price": 99, "quantity": 10, "color": "Beige", "discount": 50 },
-        { "id": 829, "name": "Waste basket", "price": 15, "quantity": 40, "color": "Silver", "discount": 0 },
-        { "id": 46, "name": "Guitar", "price": 899, "quantity": 0, "color": "Blue", "discount": 150 },
-        { "id": 17456, "name": "Matcha tea", "price": 42, "quantity": 4, "color": "Green", "discount": 11 },
-        { "id": 3292, "name": "Enlightenment", "price": 99999, "quantity": 1, "color": "Red", "discount": 0 },
-        { "id": 533, "name": "Eggs", "price": 5, "quantity": 12, "color": "Brown", "discount": 1 },
-        { "id": 683, "name": "Pillow", "price": 27, "quantity": 10, "color": "Black", "discount": 12 }
-      ];
-
       this.uk = false;
       this.currencyFormat = '$';
 
@@ -73,7 +59,7 @@
       * @param {object} item [description]
       */
      this.addNewItem = function addNewItem(item) {
-       this.items.push( {
+       this.allData.push( {
          name: item.name,
          price: Number(item.price),
          quantity: Number(item.quantity),
